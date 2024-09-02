1c7baa6c3414219efe231f2b7cb0d2caf1f32c91
Nana Quam <quamgyambrah@gmail.com>
2024-09-02 18:32:58 -0400

    [UPDATE] added datastore

--- src/main/java/com/machocoders/contacts_api/repository/ContactRepository.java
+++ src/main/java/com/machocoders/contacts_api/repository/ContactRepository.java
@@ -10,4 +10,24 @@ import com.machocoders.contacts_api.pojo.Contact;
 @Repository
 public class ContactRepository {
     private List<Contact> contacts = new ArrayList<>();
+
+    public List<Contact> getContacts() {
+        return contacts;
+    }
+
+    public Contact getContact(int index) {
+        return contacts.get(index);
+    }
+
+    public void saveContact(Contact contact) {
+        contacts.add(contact);
+    }
+
+    public void updateContact(int index, Contact contact) {
+        contacts.set(index, contact);
+    }
+
+    public void deleteContact(int index) {
+        contacts.remove(index);
+    }
 }
